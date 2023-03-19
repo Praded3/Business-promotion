1e84c3f7a9fcb9aac290b0eae80902d856d21fe1
Praded3 <125351697+Praded3@users.noreply.github.com>
2023-03-19 02:09:28 +0200

    sa

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger,.header__decorative-container").toggleClass("active");
     $(".header__burger,.header__nav").toggleClass("active");
+    $(".header__burger,header__button-item1").toggleClass("active");
     $("body").toggleClass("lock");
   });
 });
